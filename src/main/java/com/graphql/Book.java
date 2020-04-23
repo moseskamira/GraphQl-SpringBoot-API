3ce6a6802b3edd096c7bdf4ca2b0b6de74a7ec4f
moseskamira <moses.african@gmail.com>
2020-04-23 11:40:06 +0300

    Resolving Mutation

--- src/main/java/com/graphql/Book.java
+++ src/main/java/com/graphql/Book.java
@@ -78,9 +78,7 @@ public class Book {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Book book = (Book) o;
-
         return id.equals(book.id);
     }
 
